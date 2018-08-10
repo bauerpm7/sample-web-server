3d55ae61a0595616a7fb6f5d2f8ed6cf532235c9
bauerpm <bauerpm@gmail.com>
2018-08-10 18:07:49 -0400

    Added portfolio page and registered it to server

--- server.js
+++ server.js
@@ -49,6 +49,11 @@ app.get('/about', (req, res) => {
     pageTitle: 'About Page',
   })
 });
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'My Portfolio Page',
+  })
+});
 
 app.get('/bad', (req, res) => {
   res.send({
